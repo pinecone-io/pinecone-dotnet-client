c77a2825b172ce7012e09530ebfe6a01dde3e045
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-08-21 20:18:47 -0400

    :herb: Add additional properties decoding test

---
+++ src/Pinecone.Test/Integration/Serialization/AdditionalProperties.cs
@@ -0,0 +1,54 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using Pinecone;
+
+#nullable enable
+
+namespace Pinecone.Test;
+
+[TestFixture]
+public class AdditionalPropertiesTest
+{
+    [Test]
+    public void TestAdditionalProperties()
+    {
+        var inputJson =
+            @"
+        {
+  ""cloud"": ""aws"",
+  ""region"": ""us-east-1"",
+  ""extra"": ""value""
+}
+";
+        
+        
+        var expectedJson =
+            @"
+        {
+  ""cloud"": ""aws"",
+  ""region"": ""us-east-1""
+}
+";
+
+        var expectedObject = new ServerlessSpec
+        {
+            Cloud = ServerlessSpecCloud.Aws,
+            Region = "us-east-1"
+        };
+
+        var serializerOptions = new JsonSerializerOptions
+        {
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+        };
+
+        var deserializedObject = JsonSerializer.Deserialize<ServerlessSpec>(
+            inputJson,
+            serializerOptions
+        );
+
+        var serializedJson = JsonSerializer.Serialize(deserializedObject, serializerOptions);
+        Assert.That(JToken.DeepEquals(JToken.Parse(expectedJson), JToken.Parse(serializedJson)));
+    }
+}
