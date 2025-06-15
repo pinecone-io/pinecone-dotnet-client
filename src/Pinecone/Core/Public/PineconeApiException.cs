aba1032c71798c60fbe8f2b90362d5063921592a
Chris Hynes <chris@programcsharp.com>
2025-06-15 11:31:11 -0700

    Fix error handling

--- src/Pinecone/Core/Public/PineconeApiException.cs
+++ src/Pinecone/Core/Public/PineconeApiException.cs
@@ -1,3 +1,5 @@
+using Pinecone.Core;
+
 namespace Pinecone;
 
 /// <summary>
@@ -5,12 +7,16 @@ namespace Pinecone;
 /// </summary>
 public class PineconeApiException : PineconeException
 {
+    public PineconeApiException(string message, int statusCode, ErrorResponse body)
+        : this(message + (body.Error?.Message != null ? ": " + body.Error.Message : ""), statusCode, (object)body)
+    { }
+
     public PineconeApiException(string message, int statusCode, object body) : base(message)
     {
         StatusCode = statusCode;
         Body = body;        
         Data["StatusCode"] = statusCode;
-        Data["ResponseBody"] = body;
+        Data["ResponseBody"] = JsonUtils.Serialize(body);
     }
 
     /// <summary>
